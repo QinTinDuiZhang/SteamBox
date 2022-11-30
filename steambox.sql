9bafaa53b547c9d2323799a1e01edb33a368ddea
杜佳航 <114985625+DTC-007@users.noreply.github.com>
2022-11-30 15:55:54 +0800

    Add files via upload

--- steambox.sql
+++ steambox.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80019
  File Encoding         : 65001
 
- Date: 28/11/2022 15:58:21
+ Date: 30/11/2022 15:54:14
 */
 
 SET NAMES utf8mb4;
@@ -22,10 +22,10 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `admin`;
 CREATE TABLE `admin`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT,
-  `account` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `password` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `account` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '账号',
+  `password` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '密码',
+  `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '姓名',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
@@ -39,8 +39,8 @@ INSERT INTO `admin` VALUES (1, 'admin', '123456', '张三强');
 -- ----------------------------
 DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '分类名称',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
@@ -58,16 +58,16 @@ INSERT INTO `category` VALUES (5, '动作');
 -- ----------------------------
 DROP TABLE IF EXISTS `comment`;
 CREATE TABLE `comment`  (
-  `id` bigint(0) NOT NULL AUTO_INCREMENT,
-  `content` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `pubdate` datetime(0) NOT NULL,
-  `ipaddress` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `hidden` tinyint(1) NULL DEFAULT NULL,
-  `likenum` int(0) NULL DEFAULT NULL,
-  `dislikenum` int(0) NULL DEFAULT NULL,
-  `reply_for_id` bigint(0) NULL DEFAULT NULL,
-  `creator` int(0) NOT NULL,
-  `post_id` int(0) NOT NULL,
+  `id` bigint(0) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `content` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '内容',
+  `pubdate` datetime(0) NOT NULL COMMENT '发布时间',
+  `ipaddress` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT 'IP地址',
+  `hidden` tinyint(1) NULL DEFAULT NULL COMMENT '是否隐藏',
+  `likenum` int(0) NULL DEFAULT NULL COMMENT '点赞数',
+  `dislikenum` int(0) NULL DEFAULT NULL COMMENT '点踩数',
+  `reply_for_id` bigint(0) NULL DEFAULT NULL COMMENT '回复的评论编号',
+  `creator` int(0) NOT NULL COMMENT '作者编号',
+  `post_id` int(0) NOT NULL COMMENT '帖子编号',
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `reply_for_id`(`reply_for_id`) USING BTREE,
   INDEX `post_id`(`post_id`) USING BTREE,
@@ -81,7 +81,7 @@ CREATE TABLE `comment`  (
 -- Records of comment
 -- ----------------------------
 INSERT INTO `comment` VALUES (1, '这游戏就是开放世界的艺术品', '2021-12-16 22:07:41', '0:0:0:0:0:0:0:1', 0, 11, 1, NULL, 1, 1);
-INSERT INTO `comment` VALUES (2, '确实是艺术品没错', '2021-12-22 19:46:44', '0:0:0:0:0:0:0:1', 0, 0, 0, 1, 1, 1);
+INSERT INTO `comment` VALUES (2, '确实是艺术品没错', '2021-12-22 19:46:44', '0:0:0:0:0:0:0:1', 0, 0, 0, 1, 2, 1);
 INSERT INTO `comment` VALUES (3, '你是懂艺术品的', '2021-12-25 19:49:35', '0:0:0:0:0:0:0:1', 0, 1, 0, 2, 1, 1);
 
 -- ----------------------------
@@ -89,9 +89,9 @@ INSERT INTO `comment` VALUES (3, '你是懂艺术品的', '2021-12-25 19:49:35',
 -- ----------------------------
 DROP TABLE IF EXISTS `community`;
 CREATE TABLE `community`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `shoplink` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '游戏名称',
+  `shoplink` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '商店链接',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
@@ -109,15 +109,15 @@ INSERT INTO `community` VALUES (5, '艾尔登法环', 'https://store.steampowere
 -- ----------------------------
 DROP TABLE IF EXISTS `post`;
 CREATE TABLE `post`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT,
-  `title` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `img` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `content` varchar(15000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `pubdate` datetime(0) NOT NULL,
-  `click_count` int(0) NOT NULL DEFAULT 0,
-  `creator` int(0) NOT NULL,
-  `auditor` int(0) NOT NULL,
-  `community_id` int(0) NULL DEFAULT NULL,
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `title` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '标题',
+  `img` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '图片',
+  `content` varchar(15000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '内容',
+  `pubdate` datetime(0) NOT NULL COMMENT '发布时间',
+  `click_count` int(0) NOT NULL DEFAULT 0 COMMENT '阅读量',
+  `creator` int(0) NOT NULL COMMENT '作者编号',
+  `auditor` int(0) NOT NULL COMMENT '审核员编号',
+  `community_id` int(0) NULL DEFAULT NULL COMMENT '社区编号',
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `creator`(`creator`) USING BTREE,
   INDEX `auditor`(`auditor`) USING BTREE,
@@ -137,8 +137,8 @@ INSERT INTO `post` VALUES (1, '《荒野大镖客2》Steam同时在线达6万人
 -- ----------------------------
 DROP TABLE IF EXISTS `post_category`;
 CREATE TABLE `post_category`  (
-  `post_id` int(0) NOT NULL,
-  `category_id` int(0) NOT NULL,
+  `post_id` int(0) NOT NULL COMMENT '帖子编号',
+  `category_id` int(0) NOT NULL COMMENT '分类编号',
   PRIMARY KEY (`post_id`, `category_id`) USING BTREE,
   INDEX `category_id`(`category_id`) USING BTREE,
   CONSTRAINT `post_category_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
@@ -157,16 +157,16 @@ INSERT INTO `post_category` VALUES (1, 5);
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT,
-  `account` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `password` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `nickname` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `photo` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `birthday` date NULL DEFAULT NULL,
-  `email` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `mobile` char(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `regdate` datetime(0) NULL DEFAULT NULL,
-  `forbidden` tinyint(1) NULL DEFAULT NULL,
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `account` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '账号',
+  `password` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '密码',
+  `nickname` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '昵称',
+  `photo` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '头像',
+  `birthday` date NULL DEFAULT NULL COMMENT '生日',
+  `email` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '电子邮件',
+  `mobile` char(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '手机号码',
+  `regdate` datetime(0) NULL DEFAULT NULL COMMENT '注册时间',
+  `forbidden` tinyint(1) NULL DEFAULT NULL COMMENT '是否禁用',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `mobile`(`mobile`) USING BTREE,
   UNIQUE INDEX `email`(`email`) USING BTREE,
@@ -179,5 +179,25 @@ CREATE TABLE `user`  (
 INSERT INTO `user` VALUES (1, 'test', 'e1adc3949ba59abbe56e057f2f883e', 'Steve', '1.jpg', '2021-12-09', 'abc@qq.com', '15145678910', '2021-12-20 17:26:46', 0);
 INSERT INTO `user` VALUES (2, 'test1', 'e1adc3949ba59abbe56e057f2f883e', '绝地大叔', '4.jpg', '2021-12-14', 'abc123@qq.com', '15145678911', '2021-12-20 18:39:58', 0);
 INSERT INTO `user` VALUES (3, 'test233', 'e1adc3949ba59abbe56e057f2f883e', '三月寒', '2.jpg', '2022-01-18', 'abc456@qq.com', '15145678912', '2022-01-18 12:14:03', 0);
+
+-- ----------------------------
+-- Table structure for user_community
+-- ----------------------------
+DROP TABLE IF EXISTS `user_community`;
+CREATE TABLE `user_community`  (
+  `user_id` int(0) NOT NULL COMMENT '用户编号',
+  `community_id` int(0) NOT NULL COMMENT '社区编号',
+  PRIMARY KEY (`user_id`, `community_id`) USING BTREE,
+  INDEX `community_id`(`community_id`) USING BTREE,
+  CONSTRAINT `user_community_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `user_community_ibfk_2` FOREIGN KEY (`community_id`) REFERENCES `community` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user_community
+-- ----------------------------
+INSERT INTO `user_community` VALUES (1, 1);
+INSERT INTO `user_community` VALUES (1, 2);
+INSERT INTO `user_community` VALUES (1, 4);
 
 SET FOREIGN_KEY_CHECKS = 1;
